afd5ea3060e5ed9abdcfd46e92aaa407a76a5e0e
atul2 <atul19971@gmail.com>
2017-07-30 18:38:29 +0530

    First Commit

---
+++ src/main/java/com/dishtavar/cloud/dishtavarservice/DishtavarServiceApplication.java
@@ -0,0 +1,24 @@
+package com.dishtavar.cloud.dishtavarservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class DishtavarServiceApplication {
+
+	@Value("${service.instance.name}")
+	private String instance;
+	public static void main(String[] args) {
+		SpringApplication.run(DishtavarServiceApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String message() {
+		return "Hello from " + instance;
+		
+	}
+}
---
+++ src/test/java/com/dishtavar/cloud/dishtavarservice/DishtavarServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dishtavar.cloud.dishtavarservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DishtavarServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
